311c9c01b4d1c20166a65c068bd29f564a45ba59
Alexandreaam <alex.al.mar@hotmail.com>
2019-12-30 01:19:36 +0000

    Better sensor reading added with PointCloud

--- Assignment_3_and_4/src/cat_behaviour/src/cat_behaviour.cpp
+++ Assignment_3_and_4/src/cat_behaviour/src/cat_behaviour.cpp
@@ -1,37 +1,28 @@
-#include <ros/ros.h>
-#include <std_msgs/Float64.h>
-#include <geometry_msgs/Pose2D.h>
-#include <geometry_msgs/Point.h>
-#include <geometry_msgs/Twist.h>
-#include <sensor_msgs/LaserScan.h>
-#include <sensor_msgs/Range.h>
+#include "cat_behaviour.h"
 
 using namespace std;
+
+void callback(const sensor_msgs::PointCloud& str)
+{
+	cout << str << endl;
+}
 
 int main(int argc, char ** argv)
 {
 	cout << "Starting Cat Behaviour in C++" << endl;
 
-	string leftWheel_topic;
-	string rightWheel_topic;
-	string laser_topic;
-
-	leftWheel_topic = string("/cat/leftWheel_effort_controller/command");
-	rightWheel_topic = string("/cat/rightWheel_effort_controller/command");
-	laser_topic = string("/cat/laser/scan");
-
-	cout << leftWheel_topic.c_str() << endl;
-	cout << rightWheel_topic.c_str() << endl;
-
-	cout << "Building Message" << endl;
+	string leftWheel_topic = string("/cat/leftWheel_effort_controller/command");
+	string rightWheel_topic = string("/cat/rightWheel_effort_controller/command");
+	string laser_topic = string("/cat/laser/scan");
 
 	ros::init(argc, argv, "cat");
 	ros::NodeHandle nh;
-	//Listener listener;
-	//ros::Publisher lasers = n.advertise<geometry_msgs::Twist>(speeds_topic.c_str(), 1);
-	//ros::Subscriber sub = nh.subscribe(laser_topic.c_str(), 1, &listener::callback, &listener);
+
+	ros::Subscriber sub = nh.subscribe(laser_topic.c_str(), 1, callback);
 	ros::Publisher pub = nh.advertise<std_msgs::Float64>(leftWheel_topic.c_str() , 1);
 	ros::Publisher pub2 = nh.advertise<std_msgs::Float64>(rightWheel_topic.c_str() , 1);
+
+	cout << "Building Message" << endl;
 
 	std_msgs::Float64 message;
 	message.data = 100;
@@ -47,3 +38,5 @@ int main(int argc, char ** argv)
 	}
 
  }
+
+
