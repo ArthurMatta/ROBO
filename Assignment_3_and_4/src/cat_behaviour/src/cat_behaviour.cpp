ebe39b64aa75c08383ae56b9d4dc5bd959adc897
Alexandreaam <alex.al.mar@hotmail.com>
2019-12-30 19:47:46 +0000

    Code cleanup

--- Assignment_3_and_4/src/cat_behaviour/src/cat_behaviour.cpp
+++ Assignment_3_and_4/src/cat_behaviour/src/cat_behaviour.cpp
@@ -2,39 +2,41 @@
 
 using namespace std;
 
-void callback(const sensor_msgs::PointCloud& str)
-{
-	cout << str << endl;
-}
+namespace cat_robot{
 
-int main(int argc, char ** argv)
-{
-	cout << "Starting Cat Behaviour in C++" << endl;
+	CatRobot::CatRobot(int argc, char **argv){
+		cout << "Starting Cat Behaviour in C++" << endl;
 
-	string leftWheel_topic = string("/cat/leftWheel_effort_controller/command");
-	string rightWheel_topic = string("/cat/rightWheel_effort_controller/command");
-	string laser_topic = string("/cat/laser/scan");
+		leftSpinner_topic = string("/cat/leftSpinner_effort_controller/command");
+		rightSpinner_topic = string("/cat/rightSpinner_effort_controller/command");
+		backleftWheel_topic = string("/cat/leftBWheel_effort_controller/command");
+		backrightWheel_topic = string("/cat/righBtWheel_effort_controller/command");
+		laser_topic = string("/cat/laser/scan");
 
-	ros::init(argc, argv, "cat");
-	ros::NodeHandle nh;
+		
+		sub = nh.subscribe(laser_topic.c_str(), 1, &CatRobot::callback, this);
+		backL = nh.advertise<std_msgs::Float64>(backleftWheel_topic.c_str() , 1);
+		backR = nh.advertise<std_msgs::Float64>(backrightWheel_topic.c_str() , 1);
+		spinL = nh.advertise<std_msgs::Float64>(backleftWheel_topic.c_str() , 1);
+		spinR = nh.advertise<std_msgs::Float64>(backrightWheel_topic.c_str() , 1);
+	}
 
-	ros::Subscriber sub = nh.subscribe(laser_topic.c_str(), 1, callback);
-	ros::Publisher pub = nh.advertise<std_msgs::Float64>(leftWheel_topic.c_str() , 1);
-	ros::Publisher pub2 = nh.advertise<std_msgs::Float64>(rightWheel_topic.c_str() , 1);
+	void CatRobot::callback(const sensor_msgs::PointCloud& str){
+		//cout << str << endl;
+		//cout << "Building Message" << endl;
 
-	cout << "Building Message" << endl;
+		std_msgs::Float64 message;
+		message.data = 20;
 
-	std_msgs::Float64 message;
-	message.data = 20;
+		ros::Rate(200);
+		//cout << "Publishing Message" << endl;
 
-	ros::Rate(200);
-	cout << "Publishing Message" << endl;
+		backL.publish(message);
+		backR.publish(message);
+		spinL.publish(message);
+		spinR.publish(message);
 
-	while(ros::ok)
-	{
-	   pub.publish(message);
-	   pub2.publish(message);
-	   ros::spinOnce();               
+		ros::spinOnce();               
 	}
  }
 
