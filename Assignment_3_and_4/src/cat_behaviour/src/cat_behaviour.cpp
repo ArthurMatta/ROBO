4d99cfddf500a20203937cb28f9cf9c96c47e214
Alexandreaam <alex.al.mar@hotmail.com>
2019-12-31 02:26:22 +0000

    Laser data gathering and PointCloud implementation

--- Assignment_3_and_4/src/cat_behaviour/src/cat_behaviour.cpp
+++ Assignment_3_and_4/src/cat_behaviour/src/cat_behaviour.cpp
@@ -13,28 +13,71 @@ namespace cat_robot{
 		backrightWheel_topic = string("/cat/righBtWheel_effort_controller/command");
 		laser_topic = string("/cat/laser/scan");
 
-		
 		sub = nh.subscribe(laser_topic.c_str(), 1, &CatRobot::callback, this);
 		backL = nh.advertise<std_msgs::Float64>(backleftWheel_topic.c_str() , 1);
 		backR = nh.advertise<std_msgs::Float64>(backrightWheel_topic.c_str() , 1);
-		spinL = nh.advertise<std_msgs::Float64>(backleftWheel_topic.c_str() , 1);
-		spinR = nh.advertise<std_msgs::Float64>(backrightWheel_topic.c_str() , 1);
+		spinL = nh.advertise<std_msgs::Float64>(leftSpinner_topic.c_str() , 1);
+		spinR = nh.advertise<std_msgs::Float64>(rightSpinner_topic.c_str() , 1);
 	}
 
-	void CatRobot::callback(const sensor_msgs::PointCloud& str){
-		//cout << str << endl;
-		//cout << "Building Message" << endl;
+	void CatRobot::callback(const sensor_msgs::PointCloud& msg){
+		
+		//cout << msg << endl;
+
+		scan = msg;
+
+		//cout << scan.points.size() << endl;
+		
+		bool top = false, mid = false, bot = false;
+
+		for(int i = 0 ; i < scan.points.size() ; i++){
+			double x = scan.points[i].x;
+			double y = scan.points[i].y;
+			double dist = sqrt(pow(x, 2) + pow(y, 2));
+			if(i < 200){
+				if(dist < 0.2){
+					bot = true;
+				}
+			}
+			else if (i >= 200 && i < 400){
+				if(dist < 0.6){
+					mid = true;
+				}
+			}
+			else if (i >= 400 && i < 600){
+				if(dist < 2.6){
+					top = true;
+					cout << dist << endl;
+				}
+			}
+		}
+		string final;
+		if(bot || mid || top){
+			if(bot)
+				final.append("Mouse Detected NEAR!");
+			if(mid)
+				final.append("Mouse Detected Medium Range!");
+			if(top)
+				final.append("Wall Detected!");
+			cout << final << endl;
+		}
+		else{
+			cout << "All Clear" << endl;
+		}
+		bot = false;
+		mid = false;
+		top = false;
 
 		std_msgs::Float64 message;
-		message.data = 20;
+		message.data = 50;
 
 		ros::Rate(200);
 		//cout << "Publishing Message" << endl;
 
-		backL.publish(message);
-		backR.publish(message);
-		spinL.publish(message);
-		spinR.publish(message);
+		//backL.publish(message);
+		//backR.publish(message);
+		//spinL.publish(message);
+		//spinR.publish(message);
 
 		ros::spinOnce();               
 	}
